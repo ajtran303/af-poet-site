a4405944e3e03045e7db14f4b676cff392eb533d
AJ Tran <ajtrandev@gmail.com>
2020-08-07 17:14:02 -0600

    Add navbar spec

---
+++ spec/features/navbar/navbar_links_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "Navbar Links Spec" do
+  describe "As a visitor" do
+    describe "On the welcome page" do
+      it "There is a navbar" do
+        visit root_path
+
+        within ".navbar" do
+          expect(page).to have_link("Poems", href: "/poems")
+          expect(page).to have_link("About", href: "/about")
+          expect(page).to have_link("Events", href: "/events")
+          expect(page).to have_link("Media", href: "/media")
+        end
+      end
+    end
+  end
+end
