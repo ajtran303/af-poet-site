884306119a05a2e0ad855f0438e42375d542b764
AJ Tran <ajtrandev@gmail.com>
2020-08-18 03:55:15 -0600

    Add feature: admin can update about page

--- app/controllers/admin/about_controller.rb
+++ app/controllers/admin/about_controller.rb
@@ -1,5 +1,20 @@
-class Admin::AboutController < ApplicationController
+class Admin::AboutController < Admin::BaseController
   def index
     @about = About.first
+  end
+
+  def edit
+    @about = About.first
+  end
+
+  def update
+    About.first.update(about_params)
+    redirect_to admin_about_index_path
+  end
+
+  private
+
+  def about_params
+    params.require(:about).permit(:description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :dashboard, only: :index
-    resources :about, only: :index
+    resources :about, only: [:index, :edit, :update]
   end
 end
